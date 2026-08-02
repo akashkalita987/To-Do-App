945f51ec895096e72f34b498e8cea4fa9f818f42
akashkalita987 <akashjyotikalita76@gmail.com>
2026-08-02 22:31:49 +0530

    Stateful widgets

--- lib/MyHomePage.dart
+++ lib/MyHomePage.dart
@@ -1,3 +1,22 @@
 import 'package:flutter/material.dart';
 
-class MyHomePage extends StatefulWidget
+class MyHomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyHomePage();
+  }
+}
+
+class _MyHomePage extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("To Do List"),
+        backgroundColor: Colors.amber,
+      ),
+      body: ,
+    );
+  }
+}
