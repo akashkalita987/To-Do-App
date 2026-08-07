e56633267c61ae5fe47ce36948685444aab9d6a9
akashkalita987 <akashjyotikalita76@gmail.com>
2026-08-07 20:14:09 +0530

    class ready

--- lib/widgets/cls.dart
+++ lib/widgets/cls.dart
@@ -3,5 +3,11 @@ class vehical {
   int modelYear;
 
   vehical(this.name, this.modelYear);
-
+  void isValid() {
+    if (modelYear > 2000) {
+      print("valid car");
+    } else {
+      print("not valid");
+    }
+  }
 }
