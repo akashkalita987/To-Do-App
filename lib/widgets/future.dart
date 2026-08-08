16416e5bbea6d9d983abd79a3f18bf3663cc2131
akashkalita987 <akashjyotikalita76@gmail.com>
2026-08-08 20:39:29 +0530

    done

--- lib/widgets/future.dart
+++ lib/widgets/future.dart
@@ -1,5 +1,13 @@
 Future<String> fetchGreeting() {
-  return Future.delayed(const Duration(seconds: 2), () {
+  return Future.delayed(const Duration(seconds: 10), () {
     return 'hello from future';
   });
+}
+
+void main() {
+  print("Start");
+  fetchGreeting().then((message) {
+    print(message);
+  });
+  print("end");
 }
